832ffd348bca9b1195135c8ba6985ca024d0f7a5
cecygomez95 <cecygomezroldan18@gmail.com>
2015-11-30 09:12:32 -0600

    EVIDENCIA

---
+++ evaluacion.cpp
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int arreglo[20];
+int i;
+int j;
+int mayor=0;
+int masgrande=60000;
+
+int main()
+{
+    for(j=0;j<20;i++);
+    {
+    printf ("DAME UN NUMERO\N");
+    scanf("%d",arreglo[i]);
+    }
+    
+    
+    for (i=0;i<20;i++);
+    {
+    mayor=0;
+    
+    if(arreglo[i]>mayor&&arreglo[i]<masgrande)
+    mayor=arreglo[i];
+    }
+    
+    if(arreglo[i]=mayor)
+    {
+    mayor==arreglo[i];
+    }
+    printf("NUMERO MAYOR ES:%d/n,mayor");
+    masgrande=mayor;
+    
+    system("pause");
+}
